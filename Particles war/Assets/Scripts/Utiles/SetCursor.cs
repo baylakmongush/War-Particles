3c62616a2c9677843b0d6e44c44015b08791570a
Baylak <baylak@MacBook-Pro.local>
2020-09-11 21:49:13 +0700

    add close version particles war

---
+++ Particles war/Assets/Scripts/Utiles/SetCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCursor : MonoBehaviour
+{
+    /// <summary>
+    /// Set cursor texture script
+    /// </summary>
+    public Texture2D cursorImage;
+    void Start()
+    {
+        #if UNITY_WEBGL
+                float xspot = cursorImage.width / 2;
+                float yspot = cursorImage.height / 2;
+                Vector2 hotSpot = new Vector2(xspot, yspot);
+                Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.ForceSoftware);
+        #else
+				Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.Auto);
+        #endif
+    }
+}
---
+++ Particles war/Assets/Scripts/Utiles/SoundPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Play music in scene game without destroy
+/// </summary>
+public class SoundPlayer : MonoBehaviour
+{
+    public AudioSource  audioSource;
+    public static bool audioBegin;
+    private void Awake()
+    {
+        if (!audioBegin)
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+            DontDestroyOnLoad(audioSource);
+            audioBegin = true;
+        }
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name != "Game")
+        {
+            audioSource.Stop();
+            audioBegin = false;
+        }
+    }
+}
